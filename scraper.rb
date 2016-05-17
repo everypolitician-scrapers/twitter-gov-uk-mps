0164c127951c26d3eee6f3783aa1e5499a12ddc6
Chris Mytton <chrismytton@gmail.com>
2016-05-17 17:03:20 +0200

    Prefix env vars with MORPH_
    This is required for morph.io scrapers

--- scraper.rb
+++ scraper.rb
@@ -8,10 +8,10 @@ Dotenv.load
 
 def twitter
   @twitter ||= Twitter::REST::Client.new do |config|
-    config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
-    config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
-    config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
-    config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+    config.consumer_key        = ENV['MORPH_TWITTER_CONSUMER_KEY']
+    config.consumer_secret     = ENV['MORPH_TWITTER_CONSUMER_SECRET']
+    config.access_token        = ENV['MORPH_TWITTER_ACCESS_TOKEN']
+    config.access_token_secret = ENV['MORPH_TWITTER_ACCESS_TOKEN_SECRET']
   end
 end
 
